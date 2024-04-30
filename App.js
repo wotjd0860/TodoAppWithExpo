9a5d79c2e9913adb0a204a1d0640c4e0079fb892
Jaeseong Park <wotjd0860@naver.com>
2024-04-30 14:50:11 -0600

    2024-04-30 commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, 
+          Text, 
+          View, 
+          TouchableOpacity, 
+          TouchableHighlight, 
+          TouchableWithoutFeedback, 
+          Pressable } from 'react-native';
+import { theme } from "./colors"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableWithoutFeedback onPress={() => console.log("press")}>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
 }
@@ -13,8 +27,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
   },
+  header: {
+    flexDirection: "row",
+    marginTop: 100,
+    justifyContent: "space-between",
+  },
+  btnText: {
+    fontSize: 44,
+    fontWeight: "600",
+    color: "white"
+  }
 });
